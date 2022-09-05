0e7634a1e39a8982aa969423c0d05dc570bd7c0e
ohseungryeol <82445894+ohseungryeol@users.noreply.github.com>
2022-09-05 16:19:51 +0900

    팩토리얼 재귀

---
+++ src/boj10872.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class boj10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        System.out.println(factorial(n));
+    }
+
+    public static int factorial(int N){
+        if(N==0 || N==1) return 1;
+
+        return N * factorial(N - 1);
+    }
+}
