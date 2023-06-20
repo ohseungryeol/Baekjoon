2a394030fe8b1ee41d66925b8638f6cbb631db1a
ohseungryeol <rollester@naver.com>
2023-06-20 09:43:21 +0900

    .

---
+++ src/BFS/boj4179.java
@@ -0,0 +1,15 @@
+package BFS;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj4179 {
+    public static void main(String[] args) {
+        /*#: 벽
+.: 지나갈 수 있는 공간
+J: 지훈이의 미로에서의 초기위치 (지나갈 수 있는 공간)
+F: 불이 난 공간*/
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("메롱");
+    }
+}
---
+++ src/Programmers/Hash/BestAlbum.java
@@ -0,0 +1,12 @@
+package Programmers.Hash;
+
+public class BestAlbum {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+}
