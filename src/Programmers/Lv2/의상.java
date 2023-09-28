9374ec05966ef97ef23ee800636aeae4c1cb944a
ohseungryeol <rollester@naver.com>
2023-09-28 17:55:48 +0900

    시뮬레이션

---
+++ src/Programmers/Lv2/의상.java
@@ -0,0 +1,13 @@
+package Programmers.Lv2;
+
+public class 의상 {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        의상 main = new 의상();
+        main.solution(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}});
+    }
+}
