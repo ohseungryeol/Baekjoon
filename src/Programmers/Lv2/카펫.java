4d4af60ddcba444328b71fb29dc185cd221fc231
ohseungryeol <rollester@naver.com>
2023-10-03 20:19:11 +0900

    lv2

---
+++ src/Programmers/Lv2/카펫.java
@@ -0,0 +1,20 @@
+package Programmers.Lv2;
+
+public class 카펫 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        int S = brown+yellow;
+        int weight;
+        int height;
+        // S =brown+yellow;
+        // yellow = weight-2 x height-2 격자모양
+        // brown = weight x 
+        /*brwon , yellow*/
+        /*1, 9*/
+        return answer;
+    }
+    public static void main(String[] args) {
+        카펫 main = new 카펫();
+        main.solution(8, 1);
+    }
+}
